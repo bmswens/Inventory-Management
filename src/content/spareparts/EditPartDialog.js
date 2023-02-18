aff38ba67f3a483a9d1f32fab85a37721d41eb9a
Brandon Swenson <bmswens@gmail.com>
2023-02-18 00:33:01 -0700

    update spare parts

---
+++ src/content/spareparts/EditPartDialog.js
@@ -0,0 +1,111 @@
+// React
+import React from 'react'
+
+// MUI
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle, TextField } from '@mui/material'
+
+// custom
+import api from '../../api'
+
+function EditPartDialog(props) {
+    const {
+        name,
+        unit,
+        quantity,
+        bin,
+        external,
+        open,
+        close
+    } = props
+
+    const [inputName, setInputName] = React.useState(name)
+    const [inputUnit, setInputUnit] = React.useState(unit)
+    const [inputQuantity, setInputQuantity] = React.useState(quantity)
+    const [inputExternal, setInputExternal] = React.useState(external)
+
+    function handleClose() {
+        setInputName(name)
+        setInputUnit(unit)
+        setInputQuantity(quantity)
+        setInputExternal(external)
+        close()
+    }
+
+    async function submit() {
+        await api.spareParts.update(bin, {
+            name: inputName,
+            unit: inputUnit,
+            quantity: Number(inputQuantity),
+            external: inputExternal
+        })
+        handleClose()
+    }
+
+    const quantityValid = !Number.isNaN(Number(inputQuantity)) && Number(inputQuantity) >= 0
+
+    return (
+        <Dialog
+            open={open}
+            onClose={handleClose}
+            maxWidth="md"
+            fullWidth
+        >
+            <DialogTitle>
+                Edit Item
+            </DialogTitle>
+            <DialogContent>
+                <TextField
+                    fullWidth
+                    sx={{ marginTop: 1 }}
+                    label="Name"
+                    value={inputName}
+                    onChange={event => setInputName(event.target.value)}
+                />
+                <TextField
+                    fullWidth
+                    sx={{ marginTop: 1 }}
+                    label="Unit"
+                    value={inputUnit}
+                    onChange={event => setInputUnit(event.target.value)}
+                />
+                <TextField
+                    fullWidth
+                    sx={{ marginTop: 1 }}
+                    label="Bin"
+                    value={bin}
+                    disabled
+                />
+                <TextField
+                    fullWidth
+                    error={!quantityValid}
+                    sx={{ marginTop: 1 }}
+                    label="Quantity"
+                    value={inputQuantity}
+                    onChange={event => setInputQuantity(event.target.value)}
+                />
+                <TextField
+                    fullWidth
+                    sx={{ marginTop: 1 }}
+                    label="External Link"
+                    value={inputExternal}
+                    onChange={event => setInputExternal(event.target.value)}
+                />
+            </DialogContent>
+            <DialogActions>
+                <Button
+                    onClick={handleClose}
+                >
+                    Close
+                </Button>
+                <Button
+                    onClick={submit}
+                    disabled={!quantityValid}
+                >
+                    Submit
+                </Button>
+            </DialogActions>
+        </Dialog>
+    )
+}
+
+export default EditPartDialog
--- src/content/spareparts/SparePartCard.js
+++ src/content/spareparts/SparePartCard.js
@@ -2,23 +2,32 @@
 import React from 'react'
 
 // MUI
-import { Avatar, Box, Button, Card, CardActions, CardHeader, Dialog, DialogActions, DialogContent, DialogTitle, Grid, TextField, Tooltip, Typography, useTheme } from '@mui/material'
+import { Avatar, Box, Button, Card, CardActions, CardHeader, Dialog, DialogActions, DialogContent, DialogTitle, Grid, IconButton, TextField, Tooltip, Typography, useTheme } from '@mui/material'
+
+// MUI Icons
+import InfoIcon from '@mui/icons-material/Info'
+import EditIcon from '@mui/icons-material/Edit'
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart'
+import ShoppingCartCheckoutIcon from '@mui/icons-material/ShoppingCartCheckout'
 
 // react router
-import { useSearchParams } from 'react-router-dom'
+import { Link, useSearchParams } from 'react-router-dom'
 
 // custom
 import { SparePartsSearchContext } from './SpareParts'
 import api from '../../api'
+import EditPartDialog from './EditPartDialog'
+
 
 function IssuePartsDialog(props) {
     const {
-        open, 
-        close, 
-        quantity, 
-        setQuantity, 
+        open,
+        close,
+        quantity,
+        setQuantity,
         name,
-        bin
+        bin,
+        add
     } = props
     const [input, setInput] = React.useState('')
 
@@ -30,8 +39,12 @@ function IssuePartsDialog(props) {
     const isValid = !Number.isNaN(Number(input)) && Number(input) <= Number(quantity)
 
     async function submit() {
-        let current = Number(quantity) - Number(input)
-        await api.spareParts.update(bin, {quantity: current})
+        let n = Number(input)
+        if (add) {
+            n = n * -1
+        }
+        let current = Number(quantity) - n
+        await api.spareParts.update(bin, { quantity: current })
         setQuantity(current)
         handleClose()
     }
@@ -47,37 +60,37 @@ function IssuePartsDialog(props) {
                 align="center"
             >
                 <Typography variant="h4">
-                    Issue Items
+                    {add ? "Add To Stock" : "Issue Items"}
                 </Typography>
             </DialogTitle>
             <DialogContent>
                 <TextField
                     fullWidth
-                    sx={{marginTop: 1}}
+                    sx={{ marginTop: 1 }}
                     label="Name"
                     value={name}
                     disabled
                 />
                 <TextField
                     fullWidth
-                    sx={{marginTop: 1}}
+                    sx={{ marginTop: 1 }}
                     label="Bin"
                     value={bin}
                     disabled
                 />
                 <TextField
                     fullWidth
-                    sx={{marginTop: 1}}
+                    sx={{ marginTop: 1 }}
                     label="In Stock"
                     value={quantity}
                     disabled
                 />
                 <TextField
                     fullWidth
-                    sx={{marginTop: 1}}
+                    sx={{ marginTop: 1 }}
                     required
                     error={Boolean(input && !isValid)}
-                    label="Issue Amount"
+                    label="Amount"
                     value={input}
                     onChange={event => setInput(event.target.value)}
                     helperText="Must be a whole number."
@@ -112,10 +125,10 @@ function QuantityAvatar(props) {
         >
             <Avatar
                 sx={{
-                    backgroundColor: quantity ?  theme.palette.success.dark : theme.palette.error.dark
+                    backgroundColor: quantity ? theme.palette.success.dark : theme.palette.error.dark
                 }}
                 alt="quantity"
-            >   
+            >
                 {quantity}
             </Avatar>
         </Tooltip>
@@ -128,7 +141,7 @@ function SparePartHeader(props) {
         name,
         bin
     } = props
-    
+
     return (
         <CardHeader
             avatar={<QuantityAvatar quantity={quantity} />}
@@ -138,28 +151,147 @@ function SparePartHeader(props) {
     )
 }
 
+function SparePartsLink(props) {
+    const { external } = props
+
+    if (external) {
+        return (
+            <Link to={external} target="_blank">
+                <Tooltip
+                    title="More Info"
+                >
+                    <IconButton>
+                        <InfoIcon fontSize='large' />
+                    </IconButton>
+                </Tooltip>
+            </Link>
+        )
+    }
+    return (
+        <Tooltip
+            title="More Info"
+        >
+            <span>
+                <IconButton
+                    disabled={true}
+                >
+                    <InfoIcon fontSize='large' />
+                </IconButton>
+            </span>
+        </Tooltip>
+    )
+}
+
+function SparePartActions(props) {
+    const {
+        external,
+        quantity,
+        setQuantity,
+        bin,
+        name,
+        unit,
+    } = props
+
+    // dialog states
+    const [editOpen, setEditOpen] = React.useState(false)
+    const [addOpen, setAddOpen] = React.useState(false)
+    const [issueOpen, setIssueOpen] = React.useState(false)
+
+    function close() {
+        setEditOpen(false)
+        setAddOpen(false)
+        setIssueOpen(false)
+    }
+
+    // open on search
+    const [params] = useSearchParams()
+
+    React.useEffect(() => {
+        if (bin && params.get("bin") === bin) {
+            setIssueOpen(true)
+        }
+    }, [params, bin])
+
+    return (
+        <>
+            <CardActions>
+                <SparePartsLink external={external} />
+                <Tooltip
+                    title="Edit"
+                >
+                    <IconButton
+                        onClick={() => setEditOpen(true)}
+                    >
+                        <EditIcon fontSize='large' />
+                    </IconButton>
+                </Tooltip>
+                <Box sx={{ flexGrow: 1 }} />
+                <Tooltip
+                    title="Add To Stock"
+                >
+                    <IconButton
+                        onClick={() => setAddOpen(true)}
+                    >
+                        <AddShoppingCartIcon fontSize='large' />
+                    </IconButton>
+                </Tooltip>
+                <Tooltip
+                    title="Issue Items"
+                >
+                    <IconButton
+                        onClick={() => setIssueOpen(true)}
+                    >
+                        <ShoppingCartCheckoutIcon fontSize='large' />
+                    </IconButton>
+                </Tooltip>
+            </CardActions>
+            <EditPartDialog
+                open={editOpen}
+                close={close}
+                quantity={quantity}
+                setQuantity={setQuantity}
+                name={name}
+                bin={bin}
+                unit={unit}
+                external={external}
+            />
+            <IssuePartsDialog
+                add
+                open={addOpen}
+                close={close}
+                quantity={quantity}
+                setQuantity={setQuantity}
+                name={name}
+                bin={bin}
+            />
+            <IssuePartsDialog
+                open={issueOpen}
+                close={close}
+                quantity={quantity}
+                setQuantity={setQuantity}
+                name={name}
+                bin={bin}
+            />
+        </>
+    )
+
+}
+
 
 function SparePartCard(props) {
 
     const {
         quantity,
         name,
-        bin
+        bin,
+        external,
+        unit
     } = props
 
     // state
     const [stateQuantity, setStateQuantity] = React.useState(quantity)
-    const [open, setOpen] = React.useState(false)
 
-    const [params] = useSearchParams()
-
-    React.useEffect(() => {
-        if (bin && params.get("bin") === bin) {
-            setOpen(true)
-        }
-    }, [params, bin])
-
-    // search 
+    // search
     const context = React.useContext(SparePartsSearchContext)
     if (context.text) {
         let text = context.text.toLowerCase()
@@ -178,24 +310,15 @@ function SparePartCard(props) {
                     name={name}
                     bin={bin}
                 />
-                <CardActions>
-                    <Box sx={{"flexGrow": 1}} />
-                    <Button
-                        variant="contained"
-                        onClick={() => setOpen(true)}
-                    >
-                        Issue
-                    </Button>
-                </CardActions>
+                <SparePartActions
+                    quantity={stateQuantity}
+                    setQuantity={setStateQuantity}
+                    name={name}
+                    bin={bin}
+                    external={external}
+                    unit={unit}
+                />
             </Card>
-            <IssuePartsDialog
-                open={open}
-                close={() => setOpen(false)}
-                quantity={stateQuantity}
-                setQuantity={setStateQuantity}
-                name={name}
-                bin={bin}
-            />
         </Grid>
     )
 }
