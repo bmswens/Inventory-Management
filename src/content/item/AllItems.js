c7a23f6858c19b1bb3dc8c31e98b1db2d1ade621
Brandon Swenson <bmswens@gmail.com>
2023-02-28 00:48:32 -0700

    asset search added

--- src/content/item/AllItems.js
+++ src/content/item/AllItems.js
@@ -2,15 +2,82 @@
 import React from 'react'
 
 // MUI
-import { Grid } from '@mui/material'
+import { Grid, Card, CardContent, TextField, Stack, Button } from '@mui/material'
 
 // custom
 import ItemCard from './ItemCard'
 import api from '../../api'
 
+function ItemSearch(props) {
+    const { search, setSearch } = props
+    const [ text, setText ] = React.useState(search)
+    return (
+        <Grid item xs={12}>
+            <Card
+                sx={{
+                    width: "100%"
+                }}
+            >
+                <CardContent>
+                    <Stack
+                        alignItems="flex-end"
+                        spacing={1}
+                    >
+                        <TextField
+                            fullWidth
+                            type="search"
+                            label="Search"
+                            value={text}
+                            onChange={event => setText(event.target.value)}
+                            onKeyDown={event => {
+                                if (event.key === "Enter") {
+                                    setSearch(text)
+                                }
+                            }}
+                        />
+                        <Button
+                            variant="contained"
+                            onClick={() => setSearch(text)}
+                        >
+                            Search
+                        </Button>
+                    </Stack>
+                </CardContent>
+            </Card>
+        </Grid>
+    )
+}
+
 function AllItems(props) {
 
     let items = api.items.getAll()
+    const [search, setSearch] = React.useState('')
+    
+    let toDisplay = []
+    let searchKeys = [
+        "nsn",
+        "name",
+        "location",
+        "errc"
+    ]
+    if (search) {
+        for (let item of items) {
+            let shouldAdd = false
+            for (let key of searchKeys) {
+                let value = item[key]
+                if (value.includes(search)) {
+                    shouldAdd = true
+                }
+            }
+            if (shouldAdd) {
+                toDisplay.push(item)
+            }
+        }
+    }
+    else {
+        toDisplay = items
+    }
+    
 
     return (
         <Grid
@@ -22,7 +89,11 @@ function AllItems(props) {
                 marginTop: "7px"
             }}
         >
-            {items.map(item => <ItemCard key={item.nsn} {...item} />)}
+            <ItemSearch
+                search={search}
+                setSearch={setSearch}
+            />
+            {toDisplay.map(item => <ItemCard key={item.nsn} {...item} />)}
         </Grid>
     )
 }
