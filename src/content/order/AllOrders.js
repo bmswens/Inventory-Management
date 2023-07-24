38bb1a4f15b5cb2d1af648cd0ab7eed77ad38aa3
Kristopher Molter <37006238+kmolter14@users.noreply.github.com>
2023-07-24 16:24:55 -0700

    mongodb integration

--- src/content/order/AllOrders.js
+++ src/content/order/AllOrders.js
@@ -2,28 +2,120 @@
 import React from 'react'
 
 // MUI
-import { Grid } from '@mui/material'
+import { Grid, IconButton, Card, CardContent, CardActions, TextField, Stack, Button } from '@mui/material'
+import ExploreIcon from '@mui/icons-material/Explore';
 
 // custom
 import api from '../../api'
 import OrderCard from './OrderCard'
+import ItemDialog from '../../dialog/ItemDialog'
 
 function AllOrders(props) {
 
+    const [ text, setText ] = React.useState('')
+    const [search, setSearch] = React.useState('')
+    const [itemDialogOpen, setItemDialogOpen] = React.useState(false)
+
+    function close() {
+        setItemDialogOpen(false)
+    }
+
     let orders = api.orders.getAll()
 
+    let toDisplay = []
+    let searchKeys = [
+        "nsn",
+        "name",
+        "location"
+    ]
+    if (search) {
+        for (let order of orders) {
+            let shouldAdd = false
+            for (let key of searchKeys) {
+                let value = order[key]
+                if (!value) {
+                    continue
+                }
+                if (value.includes(search)) {
+                    shouldAdd = true
+                }
+            }
+            if (shouldAdd) {
+                toDisplay.push(order)
+            }
+        }
+    }
+    else {
+        toDisplay = orders
+    }
+
     return (
-        <Grid
-            container
-            spacing={1}
-            sx={{
-                paddingLeft: "7px",
-                paddingRight: "7px",
-                marginTop: "7px"
-            }}
-        >
-            {orders.map(order => <OrderCard key={order.name} {...order} />)}
-        </Grid>
+        <>
+            <div 
+                style={{
+                    paddingLeft: "7px",
+                    paddingRight: "7px",
+                    marginTop: "7px"
+                }}>
+
+                <Card>
+                    <CardContent style={{paddingBottom: "0px"}} >
+                        <TextField
+                            fullWidth
+                            type="search"
+                            label="Search"
+                            value={text}
+                            onChange={event => setText(event.target.value)}
+                            onKeyDown={event => {
+                                if (event.key === "Enter") {
+                                    setSearch(text)
+                                }
+                            }}
+                        />
+                    </CardContent>
+                    <CardActions>
+                        <Stack
+                            width='100%'
+                            direction='row'
+                            alignItems='center'
+                            justifyContent='space-between'
+                        >
+                            <IconButton
+                                onClick={() => {
+                                    setItemDialogOpen(true)
+                                }}
+                            >
+                                <ExploreIcon sx={{ fontSize: "6vmin" }} />
+                            </IconButton>
+                            <Button
+                                variant="contained"
+                                onClick={() => setSearch(text)}
+                            >
+                                Search
+                            </Button>
+                            <ItemDialog
+                                dialogBoxName="Pull Item"
+                                open={itemDialogOpen}
+                                close={close}
+                            />
+                        </Stack>
+                    </CardActions>
+                </Card>
+
+            </div>
+
+            <Grid
+                container
+                spacing={1}
+                sx={{
+                    paddingLeft: "7px",
+                    paddingRight: "7px",
+                    marginTop: "7px"
+                }}
+            >
+                {toDisplay.map(order => <OrderCard key={order.name} {...order} />)}
+            </Grid>
+        </>
     )
 }
 
--- src/content/putaway/AllPutAways.js
+++ src/content/putaway/AllPutAways.js
@@ -2,28 +2,118 @@
 import React from 'react'
 
 // MUI
-import { Grid } from '@mui/material'
+import { Grid, Card, CardContent, TextField, CardActions, Stack, IconButton, Button } from '@mui/material'
+import ExploreIcon from '@mui/icons-material/Explore';
 
 // custom
 import api from '../../api'
 import PutAwayCard from './PutAwayCard'
+import ItemDialog from '../../dialog/ItemDialog'
 
 function AllPutAways(props) {
 
+    const [ text, setText ] = React.useState('')
+    const [search, setSearch] = React.useState('')
+    const [itemDialogOpen, setItemDialogOpen] = React.useState(false)
+
+    function close() {
+        setItemDialogOpen(false)
+    }
+
     let puts = api.putAways.getAll()
 
+    let toDisplay = []
+    let searchKeys = [
+        "nsn",
+        "name",
+        "finalBin"
+    ]
+    if (search) {
+        for (let put of puts) {
+            let shouldAdd = false
+            for (let key of searchKeys) {
+                let value = put[key]
+                if (!value) {
+                    continue
+                }
+                if (value.includes(search)) {
+                    shouldAdd = true
+                }
+            }
+            if (shouldAdd) {
+                toDisplay.push(put)
+            }
+        }
+    }
+    else {
+        toDisplay = puts
+    }
+
     return (
-        <Grid
-            container
-            spacing={1}
-            sx={{
-                paddingLeft: "7px",
-                paddingRight: "7px",
-                marginTop: "7px"
-            }}
-        >
-            {puts.map(put => <PutAwayCard key={put.name} {...put} />)}
-        </Grid>
+        <>
+            <div 
+                style={{
+                    paddingLeft: "7px",
+                    paddingRight: "7px",
+                    marginTop: "7px"
+                }}>
+
+                <Card>
+                    <CardContent style={{paddingBottom: "0px"}} >
+                        <TextField
+                            fullWidth
+                            type="search"
+                            label="Search"
+                            value={text}
+                            onChange={event => setText(event.target.value)}
+                            onKeyDown={event => {
+                                if (event.key === "Enter") {
+                                    setSearch(text)
+                                }
+                            }}
+                        />
+                    </CardContent>
+                    <CardActions>
+                        <Stack
+                            width='100%'
+                            direction='row'
+                            alignItems='center'
+                            justifyContent='space-between'
+                        >
+                            <IconButton
+                                onClick={() => {
+                                    setItemDialogOpen(true)
+                                }}
+                            >
+                                <ExploreIcon sx={{ fontSize: "6vmin" }} />
+                            </IconButton>
+                            <Button
+                                variant="contained"
+                                onClick={() => setSearch(text)}
+                            >
+                                Search
+                            </Button>
+                            <ItemDialog
+                                dialogBoxName="Putaway Box"
+                                open={itemDialogOpen}
+                                close={close}
+                            />
+                        </Stack>
+                    </CardActions>
+                </Card>
+                <Grid
+                    container
+                    spacing={1}
+                    sx={{
+                        paddingLeft: "7px",
+                        paddingRight: "7px",
+                        marginTop: "7px"
+                    }}
+                >
+                    {toDisplay.map(put => <PutAwayCard key={put.name} {...put} />)}
+                </Grid>
+            </div>
+        </>
     )
 }
 
